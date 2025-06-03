7c635b18bf1dc3bc1d8e0b93654eb2bf4037e972
Guzel Brasil <arte.guzel@gmail.com>
2025-06-03 13:42:29 -0300

    Primeiro commit do sistema de gestão

---
+++ script.js
@@ -0,0 +1,4 @@
+document.addEventListener("DOMContentLoaded", () => {
+  console.log("Sistema de gestão carregado.");
+  // Aqui você poderá adicionar funções para manipular os dados futuramente
+});
