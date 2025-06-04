3a1e09af07cc82b20c1d13f5727141218f8cdb64
Guzel Brasil <arte.guzel@gmail.com>
2025-06-04 13:55:39 -0300

    atualizaçao 1 dia 4

--- script.js
+++ script.js
@@ -8,15 +8,17 @@ function mostrarFormulario() {
 form.addEventListener('submit', function (e) {
   e.preventDefault();
 
+  const item = document.getElementById('item').value;
   const descricao = document.getElementById('descricao').value;
   const quantidade = document.getElementById('quantidade').value;
-  const data = document.getElementById('data').value;
-  const previsao = document.getElementById('previsao').value;
+  const data = formatarData(document.getElementById('data').value);
+  const previsao = formatarData(document.getElementById('previsao').value);
   const status = document.getElementById('status').value;
 
   const div = document.createElement('div');
   div.className = 'solicitacao';
   div.innerHTML = `
+    <p><strong>Item:</strong> ${item}</p>
     <p><strong>Descrição:</strong> ${descricao}</p>
     <p><strong>Quantidade:</strong> ${quantidade}</p>
     <p><strong>Data da Solicitação:</strong> ${data}</p>
@@ -28,3 +30,8 @@ form.addEventListener('submit', function (e) {
   form.reset();
   form.classList.add('hidden');
 });
+
+function formatarData(dataStr) {
+  const [ano, mes, dia] = dataStr.split("-");
+  return `${dia}-${mes}-${ano}`;
+}
